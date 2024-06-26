3ccd2953ede583cab2aa7eca1a0d4b7aa349bb2f
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2024-06-25 18:10:42 -0700

    config-manager: ignore value if awsConfig returns undefined

--- config-manager.js
+++ config-manager.js
@@ -43,6 +43,9 @@ export async function processConfig(cliParameters, argv, env, isTTY) {
     if (argv[parameterKey] === undefined || argv[parameterKey] === cliParameters[parameterKey].default) {
       // Read value from `aws` cli profile configuration settings.
       const value = awsConfig.configFile[argv.awsProfile]?.[cliParameters[parameterKey].awsConfigKey];
+      if (value === undefined) {
+        continue;
+      }
       // Get expected value type.
       const type = cliParameters[parameterKey]?.type;
       // Coerce into expected value type.
